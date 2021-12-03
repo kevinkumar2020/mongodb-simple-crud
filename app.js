90441d103592a17e49c3df12f54aa11b94409559
kevin lathidadiya <kevinlathidadiya2019@gmail.com>
2021-12-03 20:55:27 +0530

    MongoDB Simple Curd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 app.use(express.json());
 const bookModel = require("./Model/book");
